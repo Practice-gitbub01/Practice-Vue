afc424f844ae47eff19250e4cc5633c95bd57916
git01 <git01T09j@gmail.com>
2025-04-26 13:21:18 +0800

    新增 script.js，增加 function

---
+++ script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            bool: false,
+            message: 'Hello Vue 3!'
+        }
+    },
+    methods: {
+        Rebool() {
+            this.bool = false
+        },
+        changeMessage() {
+            this.message = (this.bool) ? "A" : "B"
+            this.bool = !this.bool
+        },
+        vif() {
+            this.bool = !this.bool
+        }
+    }
+}).mount('#app')
